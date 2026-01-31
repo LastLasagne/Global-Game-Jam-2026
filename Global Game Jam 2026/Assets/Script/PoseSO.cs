83f3645d6a9352b47fe2e6b54f8b04548829f43c
Your Name <you@example.com>
2026-01-31 13:05:14 +0100

    Revert "Merge branch 'main' of https://github.com/LastLasagne/Global-Game-Jam-2026 into Candice-Nuts"
    This reverts commit 16eba91103cb09c49f9a8491458d8717b35b1cde, reversing
    changes made to 45976ae7b6d8e234125ec928c654368f78f917fb.

--- Global Game Jam 2026/Assets/Script/PoseSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PoseSO", menuName = "Scriptable Objects/PoseSO")]
-public class PoseSO : ScriptableObject
-{
-    public GameManager.Actor actor;
-    public GameManager.Mask mask;
-    public Vector2 position;
-}
