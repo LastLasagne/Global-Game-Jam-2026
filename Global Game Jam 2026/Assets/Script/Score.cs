ed2fa831e7a440afd7774bd94e662536a2c2e406
Lasse Norf <lasse.norf@stud.h-da.de>
2026-02-01 10:44:25 +0100

    Combo and clap VFX

--- Global Game Jam 2026/Assets/Script/Score.cs
+++ Global Game Jam 2026/Assets/Script/Score.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Score : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class Score : MonoBehaviour
     private TextMeshProUGUI text;
     private int score = 0;
     [SerializeField] private int scoreReward = 1;
+    [SerializeField] private Transform claps;
+    [SerializeField] private Transform comboVFX;
 
     private void Awake()
     {
@@ -19,12 +22,48 @@ public class Score : MonoBehaviour
     private void OnSuccess()
     {
         score += scoreReward;
-        text.text = score.ToString();
+        EvaluateScore();
     }
 
     private void OnFailure()
     {
         score = 0;
+        EvaluateScore();
+    }
+
+    private void EvaluateScore()
+    {
+        if (score == 0)
+        {
+            foreach (Transform child in claps)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+        else if (score < claps.childCount)
+        {
+            claps.GetChild(score - 1).gameObject.SetActive(true);
+        }
+
+        switch(score)
+        {
+            case 10:
+                comboVFX.transform.GetChild(0).GetComponent<VisualEffect>().Play();
+                break;
+            case 20:
+                comboVFX.transform.GetChild(1).GetComponent<VisualEffect>().Play();
+                break;
+            case 30:
+                comboVFX.transform.GetChild(2).GetComponent<VisualEffect>().Play();
+                break;
+            case 40:
+                comboVFX.transform.GetChild(3).GetComponent<VisualEffect>().Play();
+                break;
+            case 50:
+                comboVFX.transform.GetChild(4).GetComponent<VisualEffect>().Play();
+                break;
+        }
+
         text.text = score.ToString();
     }
 }
