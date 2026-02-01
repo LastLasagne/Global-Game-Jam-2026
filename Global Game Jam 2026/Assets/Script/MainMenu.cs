46817268600e482918989fc65d702bc3619c8de8
Lasse Norf <lasse.norf@stud.h-da.de>
2026-02-01 14:15:35 +0100

    Build settings, main menu

---
+++ Global Game Jam 2026/Assets/Script/MainMenu.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Button startButton;
+    private void Awake()
+    {
+        startButton = GetComponent<Button>();
+        startButton.onClick.AddListener(OnStartButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = Vector3.one * 1.1f;
+    }
+}
